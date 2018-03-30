c34c3b204d10d79fcb0f3655195e1c668f6302c3
ruimiranda <a75488@alunos.uminho.pt>
2018-03-30 19:43:46 +0100

    Nano update

---
+++ FightClub/Extra/Bot.java
@@ -0,0 +1,35 @@
+package FightClub.Extra;
+
+public class Bot {
+    double energy, x,y;
+
+    public Bot(double energy, double x, double y) {
+        this.energy = energy;
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getEnergy() {
+        return energy;
+    }
+
+    public void setEnergy(double energy) {
+        this.energy = energy;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+}
